799dbb5ad1d2e54d6f3d31b55fecdc8195827853
Phạm Hoàng Tiên <phamhoangtien4444@gmail.com>
2024-07-20 16:50:58 +0700

    .

--- source_level_1/A_star.py
+++ source_level_1/A_star.py
@@ -3,21 +3,10 @@ from best_fisrt_search import *
 def a_star_search(problem, W):
     return best_first_search(problem, lambda node: node.path_cost + W * problem.heuristic(node.state))
 
-# fileName = "test_level_1/input1_level1.txt"
-# map, start, goal = readInput(fileName)
+fileName = "test_level_1/input1_level1.txt"
+map, start, goal = readInput(fileName)
 
-matrix =   [[0, 0, 0, 0, -1, -1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, -1, 0, -1],
-            [0, 0, -1, -1, -1, 0, 0, -1, 0, -1],
-            [0, 0, 0, 0, -1, 0, 0, -1, 0, 0],
-            [0, 0, -1, -1, -1, 0, 0, -1, -1, 0],
-            [1, 0, -1, 0, 0, 0, 0, 0, -1, 0],
-            [0, 0, -2, 0, -1, 4, -1, 8, -1, 0],
-            [0, 0, 0, 0, -1, 0, 0, 0, 0, 0],
-            [0, -1, -1, -1, -1, 0, 0, 0, 0, 0],
-            [0, 0, 5, 0, 0, 0, -1, -1, -1, 0]]
-
-problem = Problem(matrix, (1, 1), (7, 8), 10, 20)
+problem = Problem(map, start, goal)
 
 result = a_star_search(problem, 1.5)
 
