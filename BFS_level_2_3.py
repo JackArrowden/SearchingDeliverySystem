ac4213acdea456551c79ba35074ae49a066fbbde
tamthwong <86826192+tamthwong@users.noreply.github.com>
2024-07-26 23:51:28 +0700

    Update BFS_level_2_3.py

--- BFS_level_2_3.py
+++ BFS_level_2_3.py
@@ -5,6 +5,8 @@ from queue import Queue
 
 def BFS_level_2_3(problem: Problem):
     node = Node(problem.init())
+    if problem.is_goal(node.state):
+        return trace_path(node)
     frontier = Queue()
     frontier.put(node)
     reached = {node.state}
@@ -12,10 +14,9 @@ def BFS_level_2_3(problem: Problem):
     while not frontier.empty():
         node = frontier.get()
 
-        if problem.is_goal(node.state):
-            return trace_path(node)
-
         for child in problem.EXPAND(node):
+            if problem.is_goal(child.state):
+                return trace_path(child)
             if child.state not in reached:
                 reached.add(child.state)
                 frontier.put(child)
